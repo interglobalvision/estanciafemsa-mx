6219cad252fe58a84c0fee72e39af3d0948425ef
Patrick Best <patrickbest@patrickbest.com>
2016-04-18 19:45:22 -0500

    scaffold archive program
    use custom render function

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,16 @@
 <?php
 
-// Custom functions (like special queries, etc)
+// RENDER functions
+
+function render_programacion_index($post_id) {
+
+  $meta = get_post_meta($post_id);
+  $img = wp_get_attachment_image_src(get_post_thumbnail_id($post_id), 'index-programacion-post');
+?>
+
+<div class="index-programacion-post u-flex-center" style="background-color: <?php if (!empty($meta['_igv_color'][0])) {echo $meta['_igv_color'][0];} ?>; background-image: url(<?php if (!empty($img)) {echo $img[0];} ?>);">
+  <?php if (!empty($meta['_igv_number'][0])) {echo $meta['_igv_number'][0];} ?>
+</div>
+
+<?php
+}
