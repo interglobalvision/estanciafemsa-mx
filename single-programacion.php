7fd105676fd4821474180d8e014c1fd258f84eb6
Michael Ray-Von <mikey@interglobal.vision>
2016-04-19 19:36:35 -0500

    rename key-color class

--- single-programacion.php
+++ single-programacion.php
@@ -24,7 +24,7 @@ if( have_posts() ) {
       </div>
       <div class="col col-1"></div>
       <div class="col col-3">
-        <header id="single-programacion-header" class="text-align-center key-color-font margin-bottom-large">
+        <header id="single-programacion-header" class="text-align-center font-key-color margin-bottom-large">
           <h3><?php if (!empty($meta['_igv_number'][0])) { echo $meta['_igv_number'][0];} ?></h3>
           <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
           <h3><?php if (!empty($meta['_igv_subtitle'][0])) { echo $meta['_igv_subtitle'][0];} ?></h3>
