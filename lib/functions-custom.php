bc0b0d92ad5dd4c74f4d2a76d19f6e235cab9d2e
Patrick Best <patrickbest@patrickbest.com>
2016-04-19 19:42:15 -0500

    add get key color function

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,51 @@
 <?php
 
 // Custom functions (like special queries, etc)
+
+function get_key_color() {
+
+  // if on single programacion post get the current post ID
+  global $post;
+  if (is_single() && is_single_type('programacion', $post)) {
+
+    $post_id = $post->ID;
+
+  } else {
+
+    // get post ID from latest programacion. if none fallback
+    $latest_programacion = get_posts(array(
+      'post_type' => 'programacion',
+      'posts_per_page' => -1,
+    ));
+
+    if (!empty($latest_programacion[0])) {
+
+      $post_id = $latest_programacion[0]->ID;
+
+    } else {
+
+      $post_id = false;
+
+    }
+
+  }
+
+  if ($post_id) {
+
+    // post color value from post meta
+    $meta = get_post_meta($post_id, '_igv_color');
+
+    if (!empty($meta[0])) {
+      $key_color = $meta[0];
+    } else {
+      $key_color = 'red';
+    }
+
+  } else {
+
+    $key_color = 'red';
+
+  }
+
+  return $key_color;
+}
