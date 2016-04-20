9c7b57576d65558a6aa38e7f0091066e92f610e4
Michael Ray-Von <mikey@interglobal.vision>
2016-04-20 14:09:08 -0500

    update query and add data

--- front-page.php
+++ front-page.php
@@ -20,28 +20,46 @@ $date = time();
       array(
         'key'     => '_igv_start_time',
         'value'   => $date,
-        'compare' => '<='
-      ),
-      array(
-        'key'     => '_igv_end_time',
-        'value'   => $date,
-        'compare' => '>='
+        'compare' => '<='  //returns current or most recent past
       )
     )
   );
   $query = new WP_Query($args);
+
   if ($query->have_posts()) {
     while ($query->have_posts()) {
       $query->the_post();
-      
+
       $meta = get_post_meta($post->ID);
 ?>
 
     <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+        <?php if (!empty($meta['_igv_start_time'][0])) { 
+            $start_time = $meta['_igv_start_time'][0];
+          ?>
+        <div>
+          <div><?php echo __('[:es]Desde[:en]From'); ?></div>
+          <div><?php echo date( 'M', $start_time ) . '. ' . date( 'd', $start_time ); ?></div>
+          <div><?php echo date( 'Y', $start_time ); ?></div>
+        </div>
+        <?php } ?>
 
-      <?php the_content(); ?>
+        <h3><?php if (!empty($meta['_igv_number'][0])) { echo 'No. ' . add_leading_zero( $meta['_igv_number'][0] ); } ?></h3>
+
+        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+        <h3><?php if (!empty($meta['_igv_subtitle'][0])) { echo $meta['_igv_subtitle'][0];} ?></h3>
+
+        <?php if (!empty($meta['_igv_end_time'][0])) { 
+            $end_time = $meta['_igv_end_time'][0];
+          ?>
+        <div>
+          <div><?php echo __('[:es]Hasta[:en]Until'); ?></div>
+          <div><?php echo date( 'M', $end_time ) . '. ' . date( 'd', $end_time ); ?></div>
+          <div><?php echo date( 'Y', $end_time ); ?></div>
+        </div>
+        <?php } ?>
 
     </article>
 
