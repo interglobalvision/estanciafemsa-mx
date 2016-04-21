6ae71dd3b5480cc2763d2d74e8e48f405f59dbcc
Michael Ray-Von <mikey@interglobal.vision>
2016-04-21 18:53:15 -0500

    update render_programacion_index to support tint to full color image

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -56,8 +56,12 @@ function render_programacion_index($post_id) {
 ?>
 
 <a href="<?php echo get_the_permalink($post_id); ?>">
-  <div class="index-programacion-post u-flex-center font-sans font-color-white" style="background-color: <?php if (!empty($meta['_igv_color'][0])) {echo $meta['_igv_color'][0];} ?>; background-image: url(<?php if (!empty($img)) {echo $img[0];} ?>);">
-    <?php if (!empty($meta['_igv_number'][0])) {echo 'No. ' . add_leading_zero( $meta['_igv_number'][0] );} ?>
+  <div class="index-programacion-post-container background-key-color">
+    <div class="index-programacion-post-image image-key-color" 
+    style="background-image: url(<?php if (!empty($img)) {echo $img[0];} ?>);"></div>
+    <div class="index-programacion-post-number u-flex-center font-sans font-color-white">
+      <?php if (!empty($meta['_igv_number'][0])) {echo 'No. ' . add_leading_zero( $meta['_igv_number'][0] );} ?>
+    </div>
   </div>
 </a>
 
