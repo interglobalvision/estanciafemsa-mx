f4fe9f9270e0c7753ccf12617bc705ec92f166f0
Patrick Best <patrickbest@patrickbest.com>
2016-04-26 20:26:38 -0500

    tidy front page template

---
+++ partials/splash.php
@@ -0,0 +1,29 @@
+<div id="scroll-buffer"></div>
+
+<div id="splash">
+  <div id="splash-container">
+    <div class="text-align-center match-height">
+      ESTANCIA FEMSA
+    </div>
+
+    <div class="text-align-center match-height">
+      CASA LUIS BARRAGÁN
+    </div>
+
+    <div class="u-holder">
+      <div class="u-held text-align-center splash-text">
+        <?php echo __('[:es]es una plataforma cultural y<br>
+        artística auspiciada por<br>
+        Casa Luis Barragán<br>
+        con el apoyo de<br>
+        Colección FEMSA.
+        [:en]es una plataforma cultural y<br>
+        artística auspiciada por<br>
+        Casa Luis Barragán<br>
+        con el apoyo de<br>
+        Colección FEMSA.');
+        ?>
+      </div>
+    </div>
+  </div>
+</div>
