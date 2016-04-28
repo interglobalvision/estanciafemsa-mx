4957272001b886844ac7bdaa5152c22d112b80dc
Patrick Best <patrickbest@patrickbest.com>
2016-04-28 18:41:44 -0500

    style 404

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@ get_header();
 <section id="main-content" class="container">
   <div class="row">
     <div class="col col-12 text-align-center">
-      <h1 class="font-bold margin-top-large margin-bottom">404 <?php echo __('[:es]No encontrado[:en]Not found'); ?></h1>
+      <h1 class="margin-top-large margin-bottom">404 <?php echo __('[:es]No encontrado[:en]Not found'); ?></h1>
     </div>
   </div>
 <!-- end main-content -->
