45912f07f66f7b9135ee548ffc1633e9a5f78d88
Patrick Best <patrickbest@patrickbest.com>
2016-04-28 18:36:52 -0500

    make all programacion numbers 2 digits min

--- single-programacion.php
+++ single-programacion.php
@@ -24,7 +24,7 @@ if( have_posts() ) {
       <div class="col col-1"></div>
       <div class="col col-2">
         <header id="single-programacion-header" class="text-align-center font-key-color margin-bottom-mid">
-          <h4 class="font-sans"><?php if (!empty($meta['_igv_number'][0])) {echo 'No. ' . $meta['_igv_number'][0];} ?></h4>
+          <h4 class="font-sans"><?php if (!empty($meta['_igv_number'][0])) {echo 'No. ' . add_leading_zero($meta['_igv_number'][0]);} ?></h4>
           <h2 class="margin-top-tiny margin-bottom-tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
           <h4 class="font-sans"><?php if (!empty($meta['_igv_subtitle'][0]) && !empty($meta['_igv_subtitle_en'][0])) {
             echo __('[:es]' . $meta['_igv_subtitle'][0] . '[:en]' . $meta['_igv_subtitle_en'][0]);
