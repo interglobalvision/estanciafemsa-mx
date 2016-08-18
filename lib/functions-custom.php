dfe009f8db90e7effba3575e6434bce78fbd897c
Patrick Best <patrickbest@patrickbest.com>
2016-08-17 19:43:19 -0500

    scaffold program archive

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,8 @@
 <?php
 
 // Custom functions (like special queries, etc)
+
+// adds leading 0 for numbers less than 2 digits
+function add_leading_zero($number) {
+  return sprintf("%02d", $number);
+}
