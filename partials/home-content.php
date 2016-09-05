156181fb8524f305fae008fb22ad319c85f08701
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-05 18:16:06 -0500

    add wrapper for home-item content. assign u-pointer to anchor link

--- partials/home-content.php
+++ partials/home-content.php
@@ -4,9 +4,9 @@ $home_content = get_post_meta( get_the_ID(), '_igv_home_content', true);
 
 if ($home_content) {
   foreach ($home_content as $item) {
-    echo '<div class="home-item u-pointer">';
+    echo '<div class="home-item"><div class="home-item-holder">';
     if (!empty($item['link'])) {
-      echo '<a href="' . get_permalink($item['link']) . '">';
+      echo '<a href="' . get_permalink($item['link']) . '" class="u-pointer">';
     }
     if (!empty($item['image_id'])) {
       echo '<div class="home-item-image-holder">' . wp_get_attachment_image($item['image_id'], 'home-thumb') . '</div>';
@@ -20,7 +20,7 @@ if ($home_content) {
     if (!empty($item['link'])) {
       echo '</a>';
     }
-    echo '</div>';
+    echo '</div></div>';
   }
 }
 
