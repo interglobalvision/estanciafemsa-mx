423659606b63950b7e1476e0bb107db70d1cbb9b
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-05 18:15:06 -0500

    update home thumb size

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -10,5 +10,5 @@ if( function_exists( 'add_image_size' ) ) {
 
   add_image_size( 'gallery', 1200, 9999, false );
 
-  add_image_size( 'home-thumb', 500, 500, false );
+  add_image_size( 'home-thumb', 550, 550, false );
 }
