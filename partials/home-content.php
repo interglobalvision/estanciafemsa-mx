2877151306d1554937231c09c8dad4db27ba93e6
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-07 18:04:42 -0500

    add event titles and image captions to home items

--- partials/home-content.php
+++ partials/home-content.php
@@ -8,16 +8,28 @@ if ($home_content) {
     if (!empty($item['image_id']) || !empty($item['event'])) {
       echo '<div class="home-item margin-top-';
       echo rand(0,1) == 1 ? 'mid' : 'basic';
-      echo '">';
+      echo !empty($item['image_id']) ? ' text-align-center">' : '">';
       if (!empty($item['event'])) {
         echo '<a href="' . get_permalink($item['event']) . '">';
       }
       if (!empty($item['image_id'])) {
         echo '<div class="home-item-image-holder">' . wp_get_attachment_image($item['image_id'], 'home-thumb') . '</div>';
+        if (!empty($item['caption']) || !empty($item['title'])) {
+          echo '<h4 class="home-item-image-caption padding-top-tiny text-align-center">';
+          echo !empty($item['title']) ? '<em>' . $item['title'] . '</em>' : '';
+          echo !empty($item['caption']) ? $item['caption'] : '';
+          echo '</h4>';
+        }
       } else if (!empty($item['event'])) {
+        $number = get_post_meta($item['event'], '_igv_number', true);
+        $subtitle = get_post_meta($item['event'], '_igv_subtitle', true);
+        $color = get_post_meta($item['event'], '_igv_color', true);
 
-        echo '<h3>';
+        echo '<h3';
+        echo !empty($color) ? ' style="color: ' . $color . '">' : '>';
+        echo 'No. ' . add_leading_zero($number) . '<br>';
         echo get_the_title($item['event']); 
+        echo !empty($subtitle) ? ', <em>' . $subtitle . '</em>' : '';
         echo '</h3>';
       }
       if (!empty($item['event'])) {
