594b6bbcb150b3bd773f37be608b4c1af072c2af
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-06 21:04:47 -0500

    update menu items

--- header.php
+++ header.php
@@ -71,44 +71,18 @@
     <div class="container padding-top-tiny">
       <div class="row">
         <div class="col col-s-12 col-m-3">
-          <h1 class="font-uppercase"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+          <h1 class="font-uppercase"><a href="<?php echo site_url('/home'); ?>"><?php bloginfo('name'); ?></a></h1>
         </div>
         <div class="col col-s-12 col-m-5">
           <ul id="header-menu" class="font-sans u-inline-list">
-            <?php
-              if (!is_page('home')) {
-            ?>
-            <li><a href="<?php echo site_url('/sobre-nosotros'); ?>">Home</a></li>
-            <?php
-              }
-              global $post;
-              if ($post && is_single_type('programacion', $post)) {
-            ?>
-            <?php
-                $programacion_posts = get_posts('post_type=programacion&posts_per_page=3');
-                if ($programacion_posts) {
-                  foreach ($programacion_posts as $programacion) {
-                    $number = get_post_meta($programacion->ID, '_igv_number', true);
-            ?>
-            <li><a href="<?php echo the_permalink($programacion->ID); ?>" <?php echo $programacion->ID == $post->ID ? 'class="font-italic"' : ''; ?>>No. <?php echo $number;?></a></li>
-            <?php
-                  }
-                }
-            ?>
-            <li><a href="<?php echo site_url('/programacion'); ?>">+</a></li>
-            <?php
-              } else if (!is_page('home') && !is_post_type_archive('programacion')) {
-            ?>
-            <li><a href="<?php echo get_the_permalink(get_id_by_slug('sobre-nosotros')); ?>#prensa">Prensa</a></li>
-            <li><a href="<?php echo get_the_permalink(get_id_by_slug('sobre-nosotros')); ?>#contacto">Contacto</a></li>
+            <li><a href="<?php echo site_url('/nosotros'); ?>">Nosotros</a></li>
+            <li><a href="<?php echo site_url('/programacion'); ?>">Programacion</a></li>
             <li><a href="<?php echo site_url('/noticias'); ?>">Noticias</a></li>
-            <?php
-              }
-            ?>
+            <li><a href="<?php echo site_url('/citas'); ?>">Citas</a></li>
           </ul>
         </div>
         <div class="col col-s-4 text-align-right only-desktop">
-          <h1 class="font-uppercase"><a href="<?php echo home_url('/citas'); ?>">Casa Luis Barragán</a></h1>
+          <h1 class="font-uppercase"><a href="<?php echo site_url('/home'); ?>">Casa Luis Barragán</a></h1>
         </div>
       </div>
     </div>
