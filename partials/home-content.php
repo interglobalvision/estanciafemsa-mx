244b327b504c08ee0f57594aa015ffa5ee9c59ef
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-07 17:17:15 -0500

    update home item

--- partials/home-content.php
+++ partials/home-content.php
@@ -5,25 +5,26 @@ $home_content = get_post_meta( get_the_ID(), '_igv_home_content', true);
 if ($home_content) {
   echo '<div id="home-holder">';
   foreach ($home_content as $item) {
-    echo '<div class="home-item margin-top-';
-    echo rand(0,1) == 1 ? 'mid' : 'basic';
-    echo '">';
-    if (!empty($item['link'])) {
-      echo '<a href="' . get_permalink($item['link']) . '">';
-    }
-    if (!empty($item['image_id'])) {
-      echo '<div class="home-item-image-holder">' . wp_get_attachment_image($item['image_id'], 'home-thumb') . '</div>';
-
-      if (!empty($item['caption'])) {
-        echo '<h4 class="home-item-image-caption padding-top-tiny text-align-center">' . $item['caption'] . '</h4>';
+    if (!empty($item['image_id']) || !empty($item['event'])) {
+      echo '<div class="home-item margin-top-';
+      echo rand(0,1) == 1 ? 'mid' : 'basic';
+      echo '">';
+      if (!empty($item['event'])) {
+        echo '<a href="' . get_permalink($item['event']) . '">';
       }
-    } else if (!empty($item['text'])) {
-      echo '<h3>' . $item['text'] . '</h3>';
+      if (!empty($item['image_id'])) {
+        echo '<div class="home-item-image-holder">' . wp_get_attachment_image($item['image_id'], 'home-thumb') . '</div>';
+      } else if (!empty($item['event'])) {
+
+        echo '<h3>';
+        echo get_the_title($item['event']); 
+        echo '</h3>';
+      }
+      if (!empty($item['event'])) {
+        echo '</a>';
+      }
+      echo '</div>';
     }
-    if (!empty($item['link'])) {
-      echo '</a>';
-    }
-    echo '</div>';
   }
   echo '</div>';
 }
