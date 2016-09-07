01147b0b00c09079a97de3e8d97f9d0cc67a2a0e
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-07 18:52:59 -0500

    update home content to accept post links. put image captions outside <a>

--- partials/home-content.php
+++ partials/home-content.php
@@ -5,35 +5,31 @@ $home_content = get_post_meta( get_the_ID(), '_igv_home_content', true);
 if ($home_content) {
   echo '<div id="home-holder">';
   foreach ($home_content as $item) {
-    if (!empty($item['image_id']) || !empty($item['event'])) {
+    if (!empty($item['link'])) {
       echo '<div class="home-item margin-top-';
       echo rand(0,1) == 1 ? 'mid' : 'basic';
       echo !empty($item['image_id']) ? ' text-align-center">' : '">';
-      if (!empty($item['event'])) {
-        echo '<a href="' . get_permalink($item['event']) . '">';
-      }
+      echo '<a href="' . get_permalink($item['link']) . '">';
       if (!empty($item['image_id'])) {
         echo '<div class="home-item-image-holder">' . wp_get_attachment_image($item['image_id'], 'home-thumb') . '</div>';
-        if (!empty($item['caption']) || !empty($item['title'])) {
-          echo '<h4 class="home-item-image-caption padding-top-tiny text-align-center">';
-          echo !empty($item['title']) ? '<em>' . $item['title'] . '</em>' : '';
-          echo !empty($item['caption']) ? $item['caption'] : '';
-          echo '</h4>';
-        }
-      } else if (!empty($item['event'])) {
-        $number = get_post_meta($item['event'], '_igv_number', true);
-        $subtitle = get_post_meta($item['event'], '_igv_subtitle', true);
-        $color = get_post_meta($item['event'], '_igv_color', true);
+      } else if (get_post_type($item['link']) == 'programacion') {
+        $number = get_post_meta($item['link'], '_igv_number', true);
+        $subtitle = get_post_meta($item['link'], '_igv_subtitle', true);
+        $color = get_post_meta($item['link'], '_igv_color', true);
 
-        echo '<h3';
+        echo '<h3 class="u-inline-block"';
         echo !empty($color) ? ' style="color: ' . $color . '">' : '>';
         echo 'No. ' . add_leading_zero($number) . '<br>';
-        echo get_the_title($item['event']); 
+        echo get_the_title($item['link']); 
         echo !empty($subtitle) ? ', <em>' . $subtitle . '</em>' : '';
         echo '</h3>';
       }
-      if (!empty($item['event'])) {
-        echo '</a>';
+      echo '</a>';
+      if (!empty($item['image_id']) && (!empty($item['caption']) || !empty($item['title']))) {
+        echo '<h4 class="home-item-image-caption padding-top-tiny text-align-center">';
+        echo !empty($item['title']) ? '<em>' . $item['title'] . '</em>' : '';
+        echo !empty($item['caption']) ? $item['caption'] : '';
+        echo '</h4>';
       }
       echo '</div>';
     }
