f5f7460056d1c8dd0e934fc5f52238175cdd5af3
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-08 13:22:01 -0500

    update single programacion markup. add gallery

--- single-programacion.php
+++ single-programacion.php
@@ -4,7 +4,7 @@ get_header();
 
 <!-- main content -->
 
-<main id="main-content" class="container">
+<main id="main-content">
 
   <!-- main posts loop -->
   <section id="programacion-post">
@@ -13,44 +13,76 @@ get_header();
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
-    $meta = get_post_meta($post->ID);
+    $gallery = get_post_meta($post->ID, '_igv_program_gallery', true);
+    $number = get_post_meta($post->ID, '_igv_number', true);
+    $credits = get_post_meta($post->ID, '_igv_credits', true);
 ?>
 
     <article <?php post_class(''); ?> id="post-<?php the_ID(); ?>">
 
-      <div class="row">
-        <div class="col col-s-12">
-          <?php if (!empty($meta['_igv_gallery'][0])) {echo do_shortcode($meta['_igv_gallery'][0]);} ?>
+      <div id="programacion-slider">
+        <div class="swiper-container gallery-<?php the_ID(); ?>"">
+          <div class="swiper-wrapper">
+<?php 
+  if (!empty($gallery)) {
+    foreach ($gallery as $image) {
+      $full = get_post_meta($image, '_igv_full_view', true);
+
+      if ($full == 'on') {
+?>
+            <div class="swiper-slide">
+              <div class="full-slide" style="background-image: url('<?php echo wp_get_attachment_image_src($image, 'full')[0]; ?>'"></div>
+            </div>
+<?php 
+      } else {
+?>
+            <div class="swiper-slide">
+              <div class="container">
+                <?php echo wp_get_attachment_image($image, 'full'); ?>
+              </div>
+            </div>
+<?php
+      }
+    }
+  } 
+?>
+          </div>
         </div>
       </div>
 
-      <header class="row">
-        <div class="col col-s-12 col-m-8">
-          <h2><?php the_title(); ?></h2>
-        </div>
-        <div class="col col-s-6 col-m-2">
-          <span class="swiper-prev u-pointer">< </span><span id="single-programacion-gallery-pagination"></span><span class="swiper-next u-pointer"> ></span>
-        </div>
-        <div class="col col-s-6 col-m-2 text-align-right">
-          <?php
-            if (!empty($meta['_igv_number'][0])) {
-              echo 'No. ' . add_leading_zero( $meta['_igv_number'][0] );
-            }
+      <div id="programacion-content">
+        <div class="container">
+          <div class="row">
+            <div class="col col-s-12 col-m-8">
+              <h2><?php the_title(); ?></h2>
+            </div>
+            <div class="col col-s-6 col-m-2">
+              <span class="swiper-prev u-pointer">< </span><span id="single-programacion-gallery-pagination"></span><span class="swiper-next u-pointer"> ></span>
+            </div>
+            <div class="col col-s-6 col-m-2 text-align-right">
+              <?php
+                previous_post_link('%link', '< ');
 
-            next_post_link('%link', ' >');
-          ?>
-        </div>
-      </header>
+                if (!empty($number)) {
+                  echo 'No. ' . add_leading_zero($number);
+                }
 
-      <div class="row">
-        <div class="col col-s-12">
-          <?php
-            the_content();
+                next_post_link('%link', ' >');
+              ?>
+            </div>
+          </div>
 
-            if (!empty($meta['_igv_credits'][0])) {
-              echo apply_filters('the_content', $meta['_igv_credits'][0]);
-            }
-          ?>
+          <div class="row">
+            <div class="col col-s-12">
+              <?php
+                the_content();
+
+                if (!empty($credits)) {
+                  echo apply_filters('the_content', $credits);
+                }
+              ?>
+            </div>
+          </div>
         </div>
       </div>
 
