198a085e70adca25158218b90cbe46e8d4469cc9
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-13 13:50:40 -0500

    fix programaction class :P

--- single-programacion.php
+++ single-programacion.php
@@ -85,7 +85,7 @@ if( have_posts() ) {
         <div class="programacion-content-holder padding-top-micro padding-bottom-micro">
           <div class="container">
             <div class="row">
-              <div class="col col-s-12 col-l-8 programaction-content">
+              <div class="col col-s-12 col-l-8 programacion-content">
                 <?php the_content(); ?>
                 <?php 
                 if (qtranxf_getLanguage() == 'es') {
