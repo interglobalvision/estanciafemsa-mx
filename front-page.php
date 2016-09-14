573c0eab7e2b7df4c52efc1dab5dbdbb5f264ff2
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-14 14:47:03 -0500

    fix front-page letterspacing

--- front-page.php
+++ front-page.php
@@ -9,12 +9,12 @@ $splash_text_en = IGV_get_option('_igv_splash_text_en');
 <!-- main content -->
 <main id="front-page" class="font-bolder">
   <a href="<?php echo site_url('/es/home'); ?>" class="front-page-block font-size-h1 line-tighter">
-    <div class="col-s-10 col-m-12 col-l-9">
+    <div class="col-l-9">
       <?php if (!empty($splash_text)) {echo $splash_text;} ?>&nbsp;>
     </div>
   </a>
   <a href="<?php echo site_url('/en/home'); ?>" class="front-page-block font-size-h1 line-tighter">
-    <div class="col-s-10 col-m-12 col-l-9">
+    <div class="col-l-9">
       <?php if (!empty($splash_text_en)) {echo $splash_text_en;} ?>&nbsp;>
     </div>
   </a>
