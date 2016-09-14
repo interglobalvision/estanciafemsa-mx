fcfef68fc286610dad82caf334ab451ab671d671
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-14 14:34:24 -0500

    shorter line-length on front-page for desktop, bigger font for header/footer/front-page, close #165, close #174

--- front-page.php
+++ front-page.php
@@ -8,11 +8,15 @@ $splash_text_en = IGV_get_option('_igv_splash_text_en');
 
 <!-- main content -->
 <main id="front-page" class="font-bolder">
-  <a href="<?php echo site_url('/es/home'); ?>" class="front-page-block">
-    <?php if (!empty($splash_text)) {echo $splash_text;} ?>&nbsp;>
+  <a href="<?php echo site_url('/es/home'); ?>" class="front-page-block font-size-h1 line-tighter">
+    <div class="col-l-10">
+      <?php if (!empty($splash_text)) {echo $splash_text;} ?>&nbsp;>
+    </div>
   </a>
-  <a href="<?php echo site_url('/en/home'); ?>" class="front-page-block">
-    <?php if (!empty($splash_text_en)) {echo $splash_text_en;} ?>&nbsp;>
+  <a href="<?php echo site_url('/en/home'); ?>" class="front-page-block font-size-h1 line-tighter">
+    <div class="col-l-10">
+      <?php if (!empty($splash_text_en)) {echo $splash_text_en;} ?>&nbsp;>
+    </div>
   </a>
   <div class="front-page-block front-page-block-bottom"></div>
   <div class="front-page-block front-page-block-bottom"></div>
