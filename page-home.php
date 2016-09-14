3d237b0e77412aaa2d260a623086f95f52b1e91e
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-14 11:58:44 -0500

    bigger white-space on right of masonry on home, close #168

--- page-home.php
+++ page-home.php
@@ -5,7 +5,7 @@ get_header();
 <!-- main content -->
 <main id="main-content" class="container">
   <div class="row">
-    <div class="col col-s-12 col-m-11 col-l-10">
+    <div class="col col-s-12 col-m-7 col-l-9">
       <?php get_template_part('partials/home-content'); ?>
     </div>
   </div>
