54df32c69f8a5f52e4ef5872816145a25e341827
Patrick Best <patrickbest@patrickbest.com>
2016-09-28 18:56:55 -0500

    scaffold search results template

---
+++ search.php
@@ -0,0 +1,66 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content" class="container">
+
+  <!-- main posts loop -->
+  <section id="results" class="row">
+
+    <div id="results-meta" class="col col-s-12 col-m-3 col-xl-2">
+      <h3 class="font-bold font-capitalize"><?php echo get_search_query(); ?></h3>
+    </div>
+
+    <div id="posts" class="col col-s-12 col-m-9 col-l-8">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+
+    $post_type = get_post_type($post->ID);
+
+    switch ($post_type) {
+      case 'programacion':
+        $post_type = '[:es]Programación[:en]Program';
+        break;
+      case 'post':
+        $post_type = '[:es]Noticias[:en]News';
+        break;
+      case 'page':
+        $post_type = '[:es]Pagina[:en]Page';
+        break;
+    }
+?>
+
+    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+      <h3 class="font-bold u-inline-block"><?php the_title(); ?></h3>&nbsp;<h4 class="font-serif u-inline-block">&nbsp;—&nbsp;</h4>&nbsp;<h4 class="font-serif font-capitalize u-inline-block"><?php echo _e($post_type); ?></h4>
+
+    </article>
+
+<?php
+  }
+} else {
+?>
+    <article class="u-alert">
+      <?php _e('[:es]Lo sentimos, no hemos encontrado lo que estás buscando[:en]Sorry, no posts matched your criteria'); ?>
+    </article>
+<?php
+} ?>
+
+      <?php get_template_part('partials/pagination'); ?>
+
+    </div>
+
+  </section>
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
