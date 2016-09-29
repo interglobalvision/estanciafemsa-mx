a2b33a44acd35fc6f30e8bcba227bfb7a4500d60
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-29 18:54:55 -0500

    add search submit graphic

--- searchform.php
+++ searchform.php
@@ -1,5 +1,7 @@
 <span id="search-toggle" class="u-pointer"><?php _e('[:es]Buscar[:en]Search'); ?></span>
 <form role="search" method="get" id="search-form" class="u-cf" action="<?php echo site_url(); ?>" autocomplete="off">
   <input id="search-input" type="text" value="" name="s">
-  <button type="submit" id="search-submit">></button>
+  <button type="submit" id="search-submit"><?php 
+    echo file_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/ui-arrow-right.svg'); 
+    ?></button>
 </form>
