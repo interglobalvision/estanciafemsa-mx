9525ba5cb90167a50149637410842ffbc5216b41
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-11-08 13:17:51 -0600

    improve legibility of meta values. add curator field data. add text time data

--- archive-programacion.php
+++ archive-programacion.php
@@ -14,32 +14,50 @@ if( have_posts() ) {
   while( have_posts() ) {
     the_post();
     $meta = get_post_meta($post->ID);
+    $color = get_post_meta($post->ID, '_igv_color', true);
+    $number = get_post_meta($post->ID, '_igv_number', true);
+    $title = get_post_meta($post->ID, '_igv_archive_title', true);
+    $subtitle = get_post_meta($post->ID, '_igv_subtitle', true);
+    $curator = get_post_meta($post->ID, '_igv_curator', true);
+    $start_time = get_post_meta($post->ID, '_igv_start_time', true);
+    $end_time = get_post_meta($post->ID, '_igv_end_time', true);
 ?>
 
     <article <?php post_class('col col-s-6 col-m-4 margin-top-small margin-bottom-small text-align-center'); ?> id="programacion-<?php the_ID(); ?>">
-      <a href="<?php the_permalink() ?>" class="programacion-item" <?php if (!empty($meta['_igv_color'][0])) { echo 'data-hover-color="' . $meta['_igv_color'][0] . '"'; } ?> >
-        <div class="margin-bottom-tiny"><?php if (!empty($meta['_igv_number'][0])) {echo 'No. ' . add_leading_zero( $meta['_igv_number'][0] );} ?></div>
+      <a href="<?php the_permalink() ?>" class="programacion-item" <?php echo !empty($number) ? 'data-hover-color="' . $number . '"' : ''; ?> >
+        <div class="margin-bottom-tiny"><?php echo !empty($number) ? 'No. ' . add_leading_zero($number) : ''; ?></div>
         <h2 class="archive-programacion-post-title"><?php 
-          if (!empty($meta['_igv_archive_title'][0])) {
-            echo apply_filters( 'the_content', $meta['_igv_archive_title'][0] );
+          if (!empty($title)) {
+            echo apply_filters( 'the_content', $title );
           } else {
             the_title(); 
           }
         ?></h2>
-        <h5 class="font-size-h4 margin-top-micro"><?php if (!empty($meta['_igv_subtitle'][0])) {echo $meta['_igv_subtitle'][0];} ?></h5>
-        <div class="margin-top-micro"><?php 
-          $start_date = false;
-          $end_date = false;
-          if (!empty($meta['_igv_start_time'][0])) {
-            $start_date = new \Moment\Moment(date('c', $meta['_igv_start_time'][0]));
+        <h4 class="margin-top-micro">
+          <?php echo !empty($subtitle) ? $subtitle : '&nbsp;'; ?>
+        </h4>
+        <h4 class="margin-top-micro">
+          <?php echo !empty($curator) ? $curator : '&nbsp;'; ?>
+        </h4>
+        <h5 class="margin-top-micro"><?php 
+          if (!empty($start_time) || !empty($end_time)) {
+            $start_date = false;
+            $end_date = false;
+            if (!empty($start_time)) {
+              $start_date = new \Moment\Moment(date('c', $start_time));
+            }
+            if (!empty($end_time)) {
+              $end_date = new \Moment\Moment(date('c', $end_time));
+            }
+            echo $start_date ? $start_date->format('d. m. y') : false;
+            echo $start_date && $end_date ? ' &mdash; ' : false;
+            echo $end_date ? $end_date->format('d. m. y') : false;
+          } elseif (!empty($text_time)) {
+            echo $text_time;
+          } else {
+            echo '&nbsp;';
           }
-          if (!empty($meta['_igv_end_time'][0])) {
-            $end_date = new \Moment\Moment(date('c', $meta['_igv_end_time'][0]));
-          }
-          echo $start_date ? $start_date->format('d. m. y') : false;
-          echo $start_date && $end_date ? ' &mdash; ' : false;
-          echo $end_date ? $end_date->format('d. m. y') : false;
-        ?></div>
+        ?></h5>
       </a>
     </article>
 
