073a733c8301ccf0fc11027c62e79da58ceab8b1
P <patrick@interglobal.vision>
2017-02-13 18:20:48 -0600

    Fix: cmb init order issue

--- functions.php
+++ functions.php
@@ -58,7 +58,7 @@ get_template_part( 'lib/theme-options' );
 
 
 // Add third party PHP libs
-add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
+add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 11 );
 function cmb_initialize_cmb_meta_boxes() {
   if (!class_exists( 'cmb2_bootstrap_202' ) ) {
     require_once 'vendor/webdevstudios/cmb2/init.php';
